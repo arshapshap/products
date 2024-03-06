e493a7c0869651c533ba780379ce44a8dfdf800e
arshapshap <arshapshap@mail.ru>
2024-03-06 15:55:57 +0300

    Create :core:presentation module (for base activity, fragment and viewmodel)

---
+++ core/presentation/src/main/java/com/arshapshap/products/core/presentation/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package com.arshapshap.products.core.presentation
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,4 @@ dependencyResolutionManagement {
 rootProject.name = "Products"
 include(":app")
 include(":feature:products")
+include(":core:presentation")
